127cee6eb2b3015f9c4bdbb8652648b5344067e3
kvasnyj <kvasnyj@gmail.com>
2018-02-08 20:59:09 +0100

    infinitife while

--- test.py
+++ test.py
@@ -4,5 +4,9 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2,GPIO.IN)
 
-i2 = GPIO.input(2)
-print(i2)
+i20 = -100
+while True:
+    i2 = GPIO.input(2)
+    if i20 != i2:
+        i20 = i2
+        print(i2)
