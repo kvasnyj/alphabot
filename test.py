7cb1946964a16b5f65bc7e4782c80eac0d5f6469
kvasnyj <kvasnyj@gmail.com>
2018-02-08 21:03:13 +0100

    take codesnippet to read humidity, temperature

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/python
+import sys
+import Adafruit_DHT
 
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(2,GPIO.IN)
-
-i20 = -100
 while True:
-    i2 = GPIO.input(2)
-    if i20 != i2:
-        i20 = i2
-        print(i2)
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
+    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+
+# import RPi.GPIO as GPIO
+
+
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(2,GPIO.IN)
+
+# i20 = -100
+# while True:
+#     i2 = GPIO.input(2)
+#     if i20 != i2:
+#         i20 = i2
+#         print(i2)
